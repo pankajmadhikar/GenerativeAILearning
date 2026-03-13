5bb9eaf55478cb76230ab28a00d4f9d24d1ce453
pankajnirmitee <pankaj.madhikar@nirmitee.io>
2026-03-13 15:06:37 +0530

    Initialize OpenAI chatbot project with essential files. Added .gitignore to exclude node_modules and .env, created package.json and package-lock.json for dependency management, and implemented index.js for chatbot functionality using LangChain and Google Generative AI.

---
+++ index.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import {ChatGoogleGenerativeAI} from '@langchain/google-genai';
+import { HumanMessage } from '@langchain/core/messages';
+dotenv.config();
+
+const googleModel = new ChatGoogleGenerativeAI({
+    model:"gemini-3.1-flash-lite-preview",
+    apiKey:process.env.GOOGLE_API_KEY
+})
+
+// const aiAnswer = async (qsn)=>{
+//     const response = await googleModel.invoke([
+//         new HumanMessage(qsn)
+//     ])
+//     console.log(response.content)
+// }
+
+const content = [
+    {role:"system", content:"keep your answer short and concise"},
+]
+
+const aiAnswer = async (qsn)=>{
+    content.push({role:"user", content:qsn})
+    const response = await googleModel.invoke(content)
+    content.push({role:"assistant", content:response.content})
+    console.log(response.content)
+}
+
+process.stdout.write("Ask any question :")
+process.stdin.on("data", (data)=>{
+    const qsn = data.toString().trim()
+    if(qsn === "exit"){
+        process.exit()
+    }else{
+        aiAnswer(qsn)
+    }
+})
+
+
+
